ed21e1e0d5a71be4fdb0b7095e0c2cd9df33c0a6
Mitch Alderson <mitchell@apollographql.com>
2022-08-04 14:26:30 -0700

    working

--- gateway/index.js
+++ gateway/index.js
@@ -1,21 +1,25 @@
-const {ApolloServer} = require('apollo-server');
-const {ApolloGateway} = require('@apollo/gateway');
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+const { readFileSync } = require("fs");
 
-require('dotenv').config();
+const supergraphSdl = readFileSync("../supergraph.graphql").toString();
+
+require("dotenv").config();
 
 const gateway = new ApolloGateway({
-  debug: true
+  debug: true,
+  supergraphSdl,
 });
 
 const server = new ApolloServer({
-  gateway
+  gateway,
 });
 
 server
   .listen()
-  .then(({url}) => {
+  .then(({ url }) => {
     console.log(`🚀 Gateway ready at ${url}`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err);
   });
--- subgraphs/products/resolvers.js
+++ subgraphs/products/resolvers.js
@@ -1,20 +1,37 @@
-const { products } = require('../data.json');
+const { products } = require("../data.json");
 
 const resolvers = {
   Product: {
     // this is called by the gateway when reviews requests location
     __resolveReference: (reference) => {
-      console.log('[product-sub][Product][__resolveReference] reference :\n', reference);
-      return products.find(p => p.id == reference.id);
-    }
+      console.log(
+        "[product-sub][Product][__resolveReference] reference :\n",
+        reference
+      );
+      return { id: reference.id };
+    },
   },
   Query: {
     products: (_, args, context) => {
       return products;
     },
     product: (_, args, context) => {
-      return products.find(p => p.id == args.id);
-    }
+      return products.find((p) => p.id == args.id);
+    },
+  },
+  Shipping: {
+    // this is called by the gateway when reviews requests location
+    __resolveReference: (reference) => {
+      console.log(
+        "[product-sub][Shipping][__resolveReference] reference :\n",
+        reference
+      );
+      return { id: reference.id };
+    },
+    product: (root) => {
+      console.log("[product-sub][Shipping][product] :\n", root);
+      return products.find((p) => p.id == root.id);
+    },
   },
 };
 
